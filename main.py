ef16c00e943cbef97e7d34f3cf1aa8b138a7f4fc
James <auminist3@gmail.com>
2025-06-03 16:41:47 +1200

    feat: added cli argument support

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from stats import *
+from sys import argv, exit
 
-def get_book_text():
-    with open("./books/frankenstein.txt") as f:
+def get_book_text(path_to_book):
+    with open(path_to_book) as f:
         file_contents = f.read()
         return file_contents
 
 def main():
-    num_words = count_words(get_book_text())
-    num_letters = analyze_text(get_book_text())
-
-    print("============ BOOKBOT ============")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    format_output(num_letters)
-    print("============= END ===============")
-
+    if len(argv) > 1:
+        num_words = count_words(get_book_text(argv[1]))
+        num_letters = analyze_text(get_book_text(argv[1]))
+    
+        print("============ BOOKBOT ============")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        format_output(num_letters)
+        print("============= END ===============")
+    else:
+        print('Usage: python3 main.py <path_to_book>')
+        exit(1)
 
 main()
